e2f849a5ccffa091206ed3490f812501ce56a8c1
JETZ <jeremyetzine@hotmail.com>
2019-01-23 16:56:16 +1100

    added and styled logos, edited index pages, added table to team show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_234942) do
+ActiveRecord::Schema.define(version: 2019_01_23_043023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_01_21_234942) do
     t.text "coach"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "thumbnail"
   end
 
 end
